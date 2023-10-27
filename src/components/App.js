0b8f52061d0ed95d160d702797fc02d09b1fbcf0
byron korir <byron.korir@student.moringaschool.com>
2023-10-27 12:33:52 +0300

    complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,36 @@
 // create your App component here
+import { useEffect, useState } from "react"
+import React from 'react'
+
+export default function App() {
+
+   const [image, setImage] = useState()
+   const [status, setStatus] = useState()
+   useEffect(() => {
+      setStatus(false)
+      fetch(`https://dog.ceo/api/breeds/image/random`)
+       .then((res) => res.json())
+       .then(dog => {
+         console.log(dog.status)
+         setImage(dog.message)
+         setStatus(dog.status)
+       })
+    
+   }, [])
+   
+
+  return (
+    <div>
+      {
+         status === "success"?
+          <img
+            src={image}
+            />
+         :
+         <p style={{color:"blue"}}>loading... </p>
+      }
+      
+    </div>
+  )
+}
+
